424a56f6d8f0051f04258e232779eba3c777de87
aluno <aluno@tub-depe-a1319.tubarao.ifsc.edu.br>
2022-05-26 21:41:43 -0300

    :tada: Start project

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public resultado = '';
+  public cor = '';
 
+  checkoutForm = this.formBuilder.group({
+    nota: 0,
+    frequencia: 0
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  calcularAprovacao() {
+    const nota = this.checkoutForm.controls['nota'].value;
+    const frequencia = this.checkoutForm.controls['frequencia'].value;
+
+    if (nota >= 6 && frequencia >= 75) {
+      this.resultado = 'Aprovado';
+    } else if (nota >= 4 && nota < 6 && frequencia >= 75) {
+      this.resultado = 'Recuperação';
+    } else if (nota < 4 || frequencia < 75) {
+      this.resultado = 'Reprovado';
+    }
+
+    this.corResultado();
+  }
+
+  corResultado() {
+    if (this.resultado == 'Aprovado') {
+      this.cor = 'd-label-result d-green';
+    } else if (this.resultado == 'Recuperação') {
+      this.cor =  'd-label-result d-yellow';
+    } else if (this.resultado == 'Reprovado') {
+      this.cor =  'd-label-result d-red';
+    }
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,58 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  valor1:number = 0;
+  valor2:number = 0;
+  resultado:number = 0;
 
+  constructor(public toastController: ToastController) {}
+
+  somar() {
+    this.resultado = this.valor1 + this.valor2;
+  }
+
+  dividir() {
+    if (this.valor2 === 0) {
+      this.exibirAlerta("Operação inválida");
+    } else {
+      this.resultado = this.valor1 / this.valor2;
+    }
+  }
+
+  multiplicar() {
+    this.resultado = this.valor1 * this.valor2;
+  }
+
+  subtrair() {
+    this.resultado = this.valor1 - this.valor2;
+  }
+
+  raizQuadrada() {
+    this.resultado = Math.sqrt(this.valor1);
+  }
+
+  fatorial() {
+    this.resultado 
+
+    var resultado = this.valor1;
+    var primeiroMultipicador = this.valor1 - 1;
+
+    for (var i = primeiroMultipicador; i > 1; i--) {
+      resultado *= i;
+    }
+
+    this.resultado = resultado;
+  }
+
+  async exibirAlerta(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2500,
+      position: 'middle',
+      icon: 'hand-left-outline'
+    });
+    toast.present();
+  }
+
+  
 }
