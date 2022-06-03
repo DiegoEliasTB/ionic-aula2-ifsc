774dfa4484de9e4e9c8ab35e89dba1fc0ef25321
aluno <aluno@tub-depe-a1319.tubarao.ifsc.edu.br>
2022-06-02 21:52:45 -0300

    Atividade 3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,64 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  protected mensagem: string;
+  protected desabilitaBotao: boolean = false;
+  protected tentativas: number = 8;
+  protected numero: number = 0;
+  protected numeroSecreto: number;
 
+  constructor(public toastController: ToastController) {
+    this.inicializaPartida();
+  }
+
+  inicializaPartida() {
+    this.desabilitaBotao = false;
+    this.numeroSecreto = this.geraNumeroSecreto();
+    this.tentativas = 8;
+    this.mensagem = '';
+  }
+
+  adivinharNumero() {
+    if (this.numero === this.numeroSecreto) {
+      this.mensagem = "Acertou!";
+      this.fimPartida("VENCEU!");
+    } else if (this.numero < this.numeroSecreto) {
+      this.mensagem = "Número é maior!";
+      this.tentativas--;
+    } else  {
+      this.mensagem = "Número é menor!";
+      this.tentativas--;
+    }
+
+    if (this.tentativas === 0) {
+      this.fimPartida("FIM DE JOGO! <br>Quer jogar de novamente?");
+    }
+  }
+
+  async fimPartida(msg: string) {
+    this.desabilitaBotao = true;
+
+    const toast = await this.toastController.create({
+      message: msg,
+      buttons:[
+        {
+          text: 'Sim',
+          handler:() => {
+            this.inicializaPartida();
+          }
+        },
+        {
+          text: 'Não',
+          handler:() => {
+            this.mensagem = 'Obrigado por jogar'
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
+  geraNumeroSecreto() {
+    return Math.round(Math.random() * (100 - 1) + 1);
+  }
 }
